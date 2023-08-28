2e975c6fc4e5cc85af7df8f28d4ecdc11c3bdc1e
Pablo Girón <127716000+Kratos07p@users.noreply.github.com>
2023-08-28 11:57:04 -0600

    Add files via upload

---
+++ Parcial 1/InformacionDocumentos/InformaciónDocumento.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InformacionDocumentos
+{
+    public class InformacionDocumento : Usuario
+    {
+        public int id { get; set; }
+        public string NombreDoc { get; set; }
+        public string TipoDoc { get; set; }
+        public DateTime FechaCreacion { get; set; }
+    }
+}
